63e7c431bdc76445ad41f32487964fe8c2320ba0
Nicolas Martinez <nicolas.martinez@poincenot.com>
2023-03-31 10:38:55 -0700

    make cpus.sh script portable

--- scripts/library/cpus.sh
+++ scripts/library/cpus.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# https://stackoverflow.com/questions/45181115/portable-way-to-find-the-number-of-processors-cpus-in-a-shell-script
+portable_nproc() {
+    OS="$(uname -s)"
+    if [ "$OS" = "Linux" ]; then
+        NPROCS="$(nproc --all)"
+    elif [ "$OS" = "Darwin" ] || \
+         [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+        NPROCS="$(sysctl -n hw.ncpu)"
+    else
+        NPROCS="$(getconf _NPROCESSORS_ONLN)"  # glibc/coreutils fallback
+    fi
+    echo "$NPROCS"
+}
+
+
 function get_number_of_cpus() {
-    local NUMBER_OF_CPUS="$(nproc)"
+    local NUMBER_OF_CPUS="$(portable_nproc)"
     echo "${NUMBER_OF_CPUS}"
 }
